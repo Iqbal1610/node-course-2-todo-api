de091e66b094d956cb87e6e217dad14ffaac6bf9
iqbal rubel <iqbalict39@outlook.com>
2018-03-03 20:01:54 +0600

    Add POST/users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -51,7 +51,7 @@ UserSchema.methods.generateAuthToken = function () {
     return token;
   });
 };
-
+//model method
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
@@ -69,10 +69,28 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials=function(email,password){
+  var User=this;
+
+return   User.findOne({email}).then((user)=>{
+  if(!user){
+    return Promise.reject();
+  }
+  return new Promise((resolve,reject)=>{
+    bcrypt.compare(password,user.password,(err,res)=>{
+      if(res){
+        resolve(user);
+      }else{
+        reject();
+      }
+    });
+  });
+});
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
-
-  if (user.isModified('password')) {
+     if (user.isModified('password')) {
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
