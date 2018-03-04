056971d381a5882d542a85bdd801c466aa25fab4
iqbal rubel <iqbalict39@outlook.com>
2018-03-04 13:43:18 +0600

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,22 @@
 //setting environment both for local and heroku
 
 var env=process.env.NODE_ENV || 'development';
+//// dynamic config
+if(env==='development' || env==='test'){
+  var config=require('./config.json');
+  var envConfig=config[env];
 
-//console.log('env *****',env);
-if(env==='development'){
-  process.env.PORT=3000;
-  process.env.MONGODB_URI='mongodb://localhost:27017/TodoApp';
-}else if (env==='test') {
-    process.env.PORT=3000;
-    process.env.MONGODB_URI='mongodb://localhost:27017/TodoAppTest';
+Object.keys(envConfig).forEach((key)=>{
+  process.env[key]=envConfig[key];
+});
 }
+
+//// loaclly config
+
+// if(env==='development'){
+//   process.env.PORT=3000;
+//   process.env.MONGODB_URI='mongodb://localhost:27017/TodoApp';
+// }else if (env==='test') {
+//     process.env.PORT=3000;
+//     process.env.MONGODB_URI='mongodb://localhost:27017/TodoAppTest';
+// }
